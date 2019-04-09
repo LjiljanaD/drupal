77429c945f4a77f3c17dfea936c9d40750cbd0bd
LjiljanaD <ljiljanadesancic@yahoo.com>
2019-04-09 15:46:58 +0200

    configuring slick module

--- modules/custom/movies/src/Controller/MoviesController.php
+++ modules/custom/movies/src/Controller/MoviesController.php
@@ -13,19 +13,16 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Drupal\Core\Path\AliasManager;
 
-class MoviesController extends ControllerBase
-{
-
+class MoviesController extends ControllerBase {
   protected $entityQuery;
   protected $entityTypeManager;
   protected $request;
   protected $moviesConfig;
-
   /**
    * Constructs a DbLogController object.
    *
    */
-  public function __construct(\Drupal\Core\Entity\Query\QueryFactory $entityQuery,
+  public function __construct($entityQuery,
                               $entityTypeManager,
                               RequestStack $request,
                               ConfigFactory $configFactory,
@@ -37,12 +34,10 @@ class MoviesController extends ControllerBase
     $this->moviesConfig = $configFactory->get('movies.settings');
     $this->aliasManager = $aliasManager;
   }
-
   /**
    * {@inheritdoc}
    */
-  public static function create(ContainerInterface $container)
-  {
+  public static function create(ContainerInterface $container)  {
     return new static(
       $container->get('entity.query'),
       $container->get('entity_type.manager'),
@@ -51,42 +46,93 @@ class MoviesController extends ControllerBase
       $container->get('path.alias_manager')
     );
   }
-
   /**
    * @return array
    * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
    * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
    */
-  public function movies()
-  {
+  public function movies()  {
     $content_per_page = $this->moviesConfig->get('movies.content_per_page');
-    $current_page = $this->request->get('page');
+    $filterPage = !empty($this->request->get('filter')) ? $this->request->get('filter') : '';
+    $current_page = !empty($this->request->get('page')) ? $this->request->get('page') : '';
+    $searchFieldEntry = !empty($this->request->get('search_filter')) ? $this->request->get('search_filter') : '';
+
     $offset = $content_per_page * $current_page;
 
-    $movieIds = $this->entityQuery
-      ->get('node')
-      ->condition('type', 'movies')
-      ->range($offset, $content_per_page)->execute();
+    $moviesIDs = $this->getSortedMoviesIds($searchFieldEntry, $filterPage, $content_per_page, $offset);
+    $moviesList = $this->loadMovieList($moviesIDs);
+    $moviesIDs = $this->getSortedMoviesIds($searchFieldEntry, $filterPage, $content_per_page, $offset);
+    $total = $this->getTotal($searchFieldEntry, $filterPage, $content_per_page);
 
-    $total = $this->entityQuery
-      ->get('node')
-      ->condition('type', 'movies')
-      ->count()
-      ->execute();
-
-    $movieslist = $this->entityTypeManager->getStorage('node')->loadMultiple($movieIds);
-
+    return array(
+      '#title' => 'World of Movies',
+      '#movies' => $moviesList,
+      '#filters' => $this->getTaxonomy(),
+      '#pager' => [
+        'current' => $current_page,
+        'next' => $current_page + 1,
+        'previous' => 0,
+        'total' => $total,
+        'first' => 0,
+        'last' => 10,
+        'currentFilter' => $filterPage,
+        'searchFilter' => $searchFieldEntry,
+      ],
+      '#theme' => 'movies',
+    );
+  }
+  private function getSortedMoviesIds($searchFieldEntry, $filterPage, $content_per_page, $offset) {
+    $results = [];
+    if ((!empty($filterPage)) && !empty($searchFieldEntry)) {
+      $results = $this->entityQuery
+        ->get('node')
+        ->condition('type', 'movies')
+        ->condition('field_movies_type', $filterPage)
+        ->condition('title',$searchFieldEntry,'CONTAINS')
+        ->range($offset, $content_per_page)
+        ->execute();
+    } else if(!empty($searchFieldEntry) && empty($filterPage)) {
+      $results = $this->entityQuery
+        ->get('node')
+        ->condition('type', 'movies')
+        ->condition('title', $searchFieldEntry, 'CONTAINS')
+        ->range($offset, $content_per_page)
+        ->execute();
+    } else if(empty($searchFieldEntry) && !empty($filterPage)) {
+      $results = $this->entityQuery
+        ->get('node')
+        ->condition('type', 'movies')
+        ->condition('field_movies_type', $filterPage)
+        ->range($offset, $content_per_page)
+        ->execute();
+    } else {
+      $results = $this->entityQuery
+        ->get('node')
+        ->condition('type', 'movies')
+        ->range($offset, $content_per_page)
+        ->execute();
+    }
+    return $results;
+  }
+  private function loadMovieList($listIDs) {
+    $moviesList = $this->entityTypeManager->getStorage('node')->loadMultiple($listIDs);
+    return $this->getMovieData($moviesList);
+  }
+  private function getMovieData($moviesList) {
     $movies = [];
-    foreach ($movieslist as $movie) {
-
+    foreach ($moviesList as $movie) {
+      $actors = [];
+      $movieTypes = [];
       foreach ($movie->get('field_actors') as $actor) {
         $actors = $actor->entity->getTitle();
       }
-
-      foreach ($movie->get('field_movies_type') as $taxonomy) {
-        $movieTypes = $taxonomy->entity->getName();
+      foreach ($movie->get('field_movies_type') as $type) {
+        $movieTypes = $type->entity->getName();
       }
-
+      foreach ($movie->get('field_exampe_of_paragraph') as $paragraph){
+        $paragraphFields=$paragraph->get('field_image_for_paragraph')->entity->getFileUri();
+      }
+  $a=1;
       $movies[] = array(
         'title' => $movie->title->value,
         'description' => $movie->body->value,
@@ -96,47 +142,61 @@ class MoviesController extends ControllerBase
         'poster' => !empty($movie->field_movie_poster->entity) ? $movie->field_movie_poster->entity->getFileUri() : NULL,
       );
     }
-
-    return array(
-      '#title' => 'World of Movies',
-      '#movies' => $movies,
-      '#filters' => $this->getTaxonomy(),
-      '#pager' => [
-        'current' => $current_page,
-        'next' => $current_page + 1,
-        'previous' => 0,
-        'total' => $total / $content_per_page,
-        'first' => 0,
-        'last' => 10
-      ],
-      '#theme' => 'movies',
-    );
-
-
+    return $movies;
   }
 
-  public function getTaxonomy(){
+  private function getTotal($searchFieldEntry, $filterPage, $content_per_page) {
+    $results = 0;
+    if ((!empty($filterPage)) && !empty($searchFieldEntry)) {
+      $totalMovies = $this->entityQuery
+        ->get('node')
+        ->condition('type', 'movies')
+        ->condition('field_movies_type', $filterPage)
+        ->condition('title',$searchFieldEntry,'CONTAINS')
+        ->count()
+        ->execute();
+    } else if(!empty($searchFieldEntry) && empty($filterPage)) {
+      $results = $this->entityQuery
+        ->get('node')
+        ->condition('type', 'movies')
+        ->condition('title', $searchFieldEntry, 'CONTAINS')
+        ->count()
+        ->execute();
+    } else if(empty($searchFieldEntry) && !empty($filterPage)) {
+      $results = $this->entityQuery
+        ->get('node')
+        ->condition('type', 'movies')
+        ->condition('field_movies_type', $filterPage)
+        ->count()
+        ->execute();
+    } else {
+      $results = $this->entityQuery
+        ->get('node')
+        ->condition('type', 'movies')
+        ->count()
+        ->execute();
+    }
+    if (($results / $content_per_page)<=1) {
+      return $results / $content_per_page;
+    } else {
+      return ceil($results / $content_per_page);
+    }
+  }
 
-    $taxonomyId='type_of_movie';
-
+  /**
+   * @return array
+   */
+  private function getTaxonomy()  {
+    $taxonomyId = 'type_of_movie';
     $terms = $this->entityTypeManager->getStorage('taxonomy_term')->loadTree($taxonomyId);
-
-    $term_datas =[];
-
+    $filters = [];
     foreach ($terms as $term) {
-      $term_datas = array(
+      $filters [] = array(
         'name' => $term->name,
-        'link' => $this->aliasManager->getAliasByPath('/taxonomy/term' . $term->tid)
+        'link' => $this->aliasManager->getAliasByPath('/taxonomy/term/' . $term->tid),
+        'id' => $term->tid
       );
     }
-     return array(
-       '#term_datas' => [
-         'name' => $term-> name
-       ]
-     );
-
-
-
-
+    return $filters;
   }
 }
---
+++ themes/custom/movies/assets/js/main.js
@@ -0,0 +1,5 @@
+(function($) {
+    // Here $ is the jQuery namespace.
+    $('.multiple-items').slick();
+
+})(jQuery);
